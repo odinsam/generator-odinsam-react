4f076b5a4ed01fc534dbf46ab1164045bc6965e3
dingjijun <dingjijun@aecg.com.cn>
2021-12-30 09:31:33 +0800

    [odinsam] change env.d.ts

--- generators/app/templates/craco.config.js
+++ generators/app/templates/craco.config.js
@@ -1,16 +1,26 @@
+const CracoAntDesignPlugin = require('craco-antd');
 const CracoLessPlugin = require('craco-less');
 
 module.exports = {
     plugins: [
         {
+            plugin: CracoAntDesignPlugin
+        },
+        {
             plugin: CracoLessPlugin,
             options: {
-                lessLoaderOptions: {
-                    lessOptions: {
-                        modifyVars: { '@primary-color': '#1DA57A' },
-                        javascriptEnabled: true
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'less-loader'
                     }
-                }
+                ]
             }
         }
     ]
--- generators/app/templates/src/react-app-env.d.ts
+++ generators/app/templates/src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+declare module '*.less' {
+    const value: {
+        [key: string]: string;
+    };
+    export = value;
+}
+
+declare module '*.png';
+
+declare module '*.svg';
